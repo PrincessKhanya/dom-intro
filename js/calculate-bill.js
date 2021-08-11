b187fd5bd89fd979acec7897aebb6afbf480238b
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2021-08-11 15:34:43 +0200

    The color section initial commit

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -6,25 +6,37 @@ var billStringElement = document.querySelector(".billString");
 
 
 calculateBtn.addEventListener('click', function (){
-        var billString = billStringElement.value;
+    var billString = billStringElement.value;
       
-        var billItems = billString.split(",");
+    var billItems = billString.split(",");
         
-        var billTotal = 0;
+    var billTotal = 0;
       
-        for (var i=0;i<billItems.length;i++){
-            var billItem = billItems[i].trim();
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
 
-            if (billItem === "call"){
-                billTotal += 2.75;
-            }
-            else if (billItem === "sms"){
-                billTotal += 0.75;
-            }
+        if (billItem === "call"){
+            billTotal += 2.75;
         }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
         
-        
-        var roundedBillTotal = billTotal.toFixed(2);
-        billTotalElement.innerHTML = roundedBillTotal;
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+
+    billTotalElement.classList.remove("danger");
+    billTotalElement.classList.remove("warning");
     
+
+    if (billTotal >= 20 && billTotal<=30){
+        billTotalElement.classList.add("warning");
+    }else if (billTotal > 30){
+        billTotalElement.classList.add("danger");
+    }
+
+
+       
+        
 } );
